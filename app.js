037e317cc88913eb31e9b495d773b769a9c4dd83
Saad Farooq <faroqsaad2810@gmail.com>
2023-02-27 19:06:55 +0500

    feat: Added login/signup routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const connectDB = require("./config/database")
 const express = require("express");
+const auth = require("./middleware/auth");
+
 
 const app =express()
 
@@ -8,5 +10,13 @@ const app =express()
 connectDB()
 
 app.use (express.json())
+
+//define routes
+
+app.get('/', auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+})
+app.use('/', require('./routes/users'))
+
 
 module.exports = app
